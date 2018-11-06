c0800d1cd88af3188cec9ea7abaa0cfb18b8ff17
Owen Henley <owenhenley@me.com>
2018-11-06 14:57:49 -0700

    New Assets

---
+++ Reflect/Models/MeditationCategory.swift
@@ -0,0 +1,23 @@
+//
+//  ConstantData.swift
+//  Reflect
+//
+//  Created by Owen Henley on 11/1/18.
+//  Copyright © 2018 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+struct MeditationCategories{
+    var name: String
+    
+    static let categories = [
+        MeditationCategories(name: "Body Scan"),
+        MeditationCategories(name: "Brief Mindfulness Practices"),
+        MeditationCategories(name: "Mindful Movement"),
+        MeditationCategories(name: "Mindfulness of Breath"),
+        MeditationCategories(name: "Mountain Meditation"),
+        MeditationCategories(name: "Self Guided"),
+        MeditationCategories(name: "Sitting Meditations")
+    ]
+}
