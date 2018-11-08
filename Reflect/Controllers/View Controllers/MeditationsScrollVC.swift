b37c38c063b691f3d534356eb6ea1582c0aa94ff
Owen Henley <owenhenley@me.com>
2018-11-07 17:14:31 -0700

    Started working on switching out Table View for Scroll View
    used SYNX to organise files

---
+++ Reflect/Controllers/View Controllers/MeditationsScrollVC.swift
@@ -0,0 +1,24 @@
+//
+//  MeditationsScrollVC.swift
+//  Reflect
+//
+//  Created by Owen Henley on 11/7/18.
+//  Copyright © 2018 Owen Henley. All rights reserved.
+//
+
+import UIKit
+
+class MeditationsScrollVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       
+    }
+
+}
